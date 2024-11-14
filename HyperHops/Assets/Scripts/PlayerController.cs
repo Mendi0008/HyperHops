af35aafa4235c8bbf61b8debb6cc71e34d76a8e4
Mod <ganthony0008@gmail.com>
2024-11-14 21:06:35 +0800

    Double Jump and Slight Dash

--- HyperHops/Assets/Scripts/PlayerController.cs
+++ HyperHops/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
     public Rigidbody rb;
     private Vector3 movement;
+    public float groundDistance = 1.1f;
+    public float velocity;
+
+    //doublejump
+    private bool doubleJump;
+
+    //dashvariables
+    private bool canDash = true;
+    private bool isDashing;
+    private float Dashingpower = 20.0f;
+    private float DashingTime = 0.2f;
+    private float DashCooldown = 1f;
+    [SerializeField] TrailRenderer tr;
+
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         movement = new Vector3(moveX, 0f, moveZ).normalized * moveSpeed;
+        
+        //checker
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
 
+        //double jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded() || doubleJump)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                doubleJump = !doubleJump;
+            }
+        }
+        
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+            
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+        //dashingmovement
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
         }
     }
 
@@ -26,10 +68,29 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 newPosition = rb.position + movement * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
+
+        if(isDashing)
+        {
+            return; 
+        }    
     }
 
     bool IsGrounded()
     {
-        return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector3(transform.localScale.x * Dashingpower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(DashingTime);
+        tr.emitting = false;
+        isDashing = false;
+        yield return new WaitForSeconds(DashCooldown);
+        canDash = true;
+
     }
 }
